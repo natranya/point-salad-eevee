db833d01c4b2631c447877a0a7440ccded68ad91
natranya <kky8590@naver.com>
2022-11-06 22:31:52 +0900

    Feat: 로직 함수 분리 (#1)

--- src/page/main/Main.tsx
+++ src/page/main/Main.tsx
@@ -1,22 +1,18 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import { originCards } from "../../common/data/originCard.data";
 import {
   IBoard,
   ISelectedBoardCardIndex,
 } from "../../common/interface/board.interface";
-import {
-  ICard,
-  TCardSide,
-  TPokemonExceptEevee,
-} from "../../common/interface/card.interface";
+import { TCardSide } from "../../common/interface/card.interface";
 import { IDeck } from "../../common/interface/deck.interface";
+import { GameUtil } from "../../common/util/game.util";
+import { UserUtil } from "../../common/util/user.util";
 import BoardSection from "./components/BoardSection";
 import OpponentDeckAndMyDeckSection from "./components/OpponentDeckAndMyDeckSection";
 
 export default function Main() {
   console.log("RENREDER!!");
-  let rawCards: ICard[];
   const defaultUserDeck: IDeck = {
     pointSideCards: [],
     pokemonSide: {
@@ -38,177 +34,40 @@ export default function Main() {
     pokemonCards: [],
   });
   const [userDeck, setUserDeck] = useState<IDeck>(defaultUserDeck);
+
   const initGame = () => {
-    getOriginCards();
-    makeThreePointDummies();
-  };
-  const checkSelectAvailable = () => {
-    if (
-      selectedBoardCardIndex.pointCardIndex !== null ||
-      selectedBoardCardIndex.pokemonCardIndexs.length === 2
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-    // TODO: 남은 장수가 부족한 경우
-    // TODO: 종료 조건 체크
-  };
-  const moveSelectedCardToUserDeck = () => {
-    if (selectedBoardCardIndex.pointCardIndex !== null) {
-      const cardDummy =
-        board.pointCardDummies[selectedBoardCardIndex.pointCardIndex];
-      userDeck.pointSideCards.push(cardDummy[cardDummy.length - 1]);
-      cardDummy.splice(-1, 1);
-    } else if (selectedBoardCardIndex.pokemonCardIndexs.length !== 0) {
-      selectedBoardCardIndex.pokemonCardIndexs.forEach((cardIndex) => {
-        if (board.pokemonCards[cardIndex] !== null) {
-          userDeck.pokemonSide[
-            board.pokemonCards[cardIndex]!.pokemonSide
-              .key as TPokemonExceptEevee
-          ].push(board.pokemonCards[cardIndex]!);
-        }
-        board.pokemonCards[cardIndex] = null;
-      });
-    }
-    selectedBoardCardIndex.pointCardIndex = null;
-    selectedBoardCardIndex.pokemonCardIndexs = [];
-  };
-  const calculatePoint = () => {
-    userDeck.pointSideCards.forEach((card) => {
-      card.pointSide.calculatePoint(userDeck);
-    });
+    const rawCards = GameUtil.makeRawCards();
+    board.pointCardDummies = GameUtil.makeThreePointDummies(rawCards);
   };
 
-  const clickBoardCard = (selectedSide: TCardSide, index: number): void => {
-    if (selectedSide === "POINT_SIDE") {
-      selectedBoardCardIndex.pokemonCardIndexs = [];
-      selectedBoardCardIndex.pointCardIndex =
-        selectedBoardCardIndex.pointCardIndex === index ? null : index;
-    } else if (selectedSide === "POKEMON_SIDE") {
-      const findedIndex = selectedBoardCardIndex.pokemonCardIndexs.findIndex(
-        (cardIndex) => cardIndex === index
-      );
-      if (findedIndex === -1) {
-        if (selectedBoardCardIndex.pokemonCardIndexs.length < 2)
-          selectedBoardCardIndex.pokemonCardIndexs.push(index);
-      } else {
-        selectedBoardCardIndex.pokemonCardIndexs.splice(findedIndex, 1);
-      }
-      selectedBoardCardIndex.pointCardIndex = null;
-    }
-    console.log(selectedBoardCardIndex);
-    setSelectedBoardCardIndex({ ...selectedBoardCardIndex });
+  const clickBoardCard = (
+    selectedSide: TCardSide,
+    clickIndex: number
+  ): void => {
+    const newselectedBoardCardIndex = UserUtil.clickBoardCard(
+      selectedSide,
+      selectedBoardCardIndex,
+      clickIndex
+    );
+    setSelectedBoardCardIndex({ ...newselectedBoardCardIndex });
   };
 
   const selectCard = () => {
-    if (checkSelectAvailable()) {
-      moveSelectedCardToUserDeck();
-      calculatePoint();
-      fillEmptyPokemonCardOnBoard();
-      setBoard({ ...board });
-      setUserDeck({ ...userDeck });
+    if (UserUtil.checkCardSelectAvailable(selectedBoardCardIndex)) {
+      const [newBoard, newUserDeck, newSelectedBoardCardIndex] =
+        UserUtil.selectCard(board, userDeck, selectedBoardCardIndex);
+      const filledBoard = GameUtil.fillEmptyPokemonCardOnBoard(newBoard);
+      setSelectedBoardCardIndex({ ...newSelectedBoardCardIndex });
+      setBoard({ ...filledBoard });
+      setUserDeck({ ...newUserDeck });
     }
-  };
-
-  const getOriginCards = () => {
-    let temp: ICard[] = [];
-    for (let i = 0; i < 6; i++) {
-      temp = temp.concat(originCards);
-    }
-    rawCards = [...temp];
-  };
-  const makeThreePointDummies = () => {
-    if (rawCards && Array.isArray(rawCards) && rawCards.length !== 0) {
-      const cardLength = rawCards.length;
-      const mixCard = () => {
-        const firstIndex = Math.floor(Math.random() * cardLength);
-        const secondIndex = Math.floor(Math.random() * cardLength);
-        const temp = rawCards[firstIndex];
-        rawCards[firstIndex] = rawCards[secondIndex];
-        rawCards[secondIndex] = temp;
-      };
-      for (let i = 0; i < 1000; i++) {
-        mixCard();
-      }
-      const initCardLengthPerDummy = rawCards
-        ? Math.floor(rawCards.length) / 3
-        : 0;
-      board.pointCardDummies[0] = [
-        ...rawCards.slice(0, initCardLengthPerDummy),
-      ];
-      board.pointCardDummies[1] = [
-        ...rawCards.slice(initCardLengthPerDummy, initCardLengthPerDummy * 2),
-      ];
-      board.pointCardDummies[2] = [
-        ...rawCards.slice(initCardLengthPerDummy * 2),
-      ];
-    } else {
-      board.pointCardDummies = [[], [], []];
-    }
-    setBoard(board);
-  };
-
-  const fillEmptyPokemonCardOnBoard = () => {
-    const checkPointDummy = (index: number): boolean => {
-      if (
-        board.pointCardDummies[index] &&
-        board.pointCardDummies[index].length > 0
-      ) {
-        return true;
-      } else {
-        return false;
-      }
-    };
-
-    const getCardFromDummy = (index: number) => {
-      const cardDummy = board.pointCardDummies[index % 3];
-      board.pokemonCards[index] = cardDummy[cardDummy.length - 1];
-      cardDummy.splice(-1, 1);
-    };
-
-    const fillDummyFromOtherDummy = (index: number) => {
-      let dummyIndexForFill = -1;
-      let maxCardLength = 0;
-      board.pointCardDummies.forEach((dummy, dummyIndex) => {
-        if (
-          dummyIndex !== index &&
-          dummy.length > maxCardLength &&
-          dummy.length > 1
-        ) {
-          dummyIndexForFill = dummyIndex;
-          maxCardLength = dummy.length;
-        }
-      });
-      if (dummyIndexForFill !== -1) {
-        board.pointCardDummies[index] = board.pointCardDummies[
-          dummyIndexForFill
-        ].slice(0, Math.ceil(maxCardLength / 2));
-        board.pointCardDummies[dummyIndexForFill] = board.pointCardDummies[
-          dummyIndexForFill
-        ].slice(Math.ceil(maxCardLength / 2));
-      }
-    };
-
-    for (let i = 0; i < 6; i++) {
-      if (!board.pokemonCards[i]) {
-        if (checkPointDummy(i % 3)) {
-          getCardFromDummy(i);
-        } else {
-          fillDummyFromOtherDummy(i % 3);
-          if (checkPointDummy(i % 3)) {
-            getCardFromDummy(i);
-          }
-        }
-      }
-    }
-
-    setBoard({ ...board });
   };
 
   useEffect(() => {
     initGame();
-    fillEmptyPokemonCardOnBoard();
+    const newBoard = GameUtil.fillEmptyPokemonCardOnBoard(board);
+    setBoard({ ...newBoard });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
